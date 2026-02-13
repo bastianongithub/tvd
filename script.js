9eee2a0fb6398cbbed00e09f953d3bf9e4e2a93f
bastianongithub <162016298+bastianongithub@users.noreply.github.com>
2026-02-13 20:15:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ const yesTeasePokes = [
     "try saying no first... I bet you want to know what happens 😏",
     "Since when have you been that easy on me? 👀",
     "Really!?",
-    "click no, I dare you 😏",
-    "I double dare you"
+    "click no, I dare you 😏"
 ]
 
 let yesTeasedCount = 0
